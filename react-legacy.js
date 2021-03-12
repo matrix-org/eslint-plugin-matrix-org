91ab8a432e55bcea7fc37873b095415d8bcdb45d
J. Ryan Stinnett <jryans@gmail.com>
2021-03-12 15:57:38 +0000

    Remove Flow support

--- react-legacy.js
+++ react-legacy.js
@@ -1,6 +1,5 @@
 module.exports = {
     plugins: [
-        "flowtype",
         "matrix-org",
     ],
     extends: "plugin:matrix-org/react",
@@ -10,30 +9,10 @@ module.exports = {
         },
     },
     rules: {
-        // Flowtype
-        "flowtype/require-parameter-type": ["warn", {
-            "excludeArrowFunctions": true,
-        }],
-        "flowtype/define-flow-type": "warn",
-        "flowtype/require-return-type": ["warn",
-            "always",
-            {
-              "annotateUndefined": "never",
-              "excludeArrowFunctions": true,
-            }
-        ],
-        "flowtype/space-after-type-colon": ["warn", "always"],
-        "flowtype/space-before-type-colon": ["warn", "never"],
-
         // Bind or arrow functions in props causes performance issues (but we
         // currently use them in some places).
         // It's disabled here, but we should using it sparingly.
         "react/jsx-no-bind": "off",
         "react/jsx-key": ["error"],
-    },
-    settings: {
-        flowtype: {
-            onlyFilesWithFlowAnnotation: true
-        },
     },
 }
