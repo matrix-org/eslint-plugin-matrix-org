280706be0903994122958c984f9b37b735775a4f
J. Ryan Stinnett <jryans@gmail.com>
2021-05-27 14:16:25 +0100

    Extract a separate Babel layer
    Some projects (like `element-desktop`) use native JavaScript without Babel, so
    they don't want to pull in a Babel parser just to lint. This separates native JS
    and Babel JS configs to offer both options.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     configs: {
+        "babel": require("./babel"),
         "javascript": require("./javascript"),
         "react": require("./react"),
         "typescript": require("./typescript"),
--- javascript.js
+++ javascript.js
@@ -1,36 +1,24 @@
 module.exports = {
-    plugins: [
-        "@babel",
-    ],
     extends: [
         "eslint:recommended",
         "google",
     ],
-    parser: "@babel/eslint-parser", // Needed for class properties
-    parserOptions: {
-        sourceType: "module",
-    },
-    env: {
-        // We expect all projects to use ES6 or newer
-        es6: true,
-        jest: true,
-    },
     rules: {
         // Additional rules we adhere to
         "indent": [
-          "error",
-          4,
-          {
-            SwitchCase: 1,
-            ArrayExpression: "off",
-            ObjectExpression: "off",
-          },
+            "error",
+            4,
+            {
+                SwitchCase: 1,
+                ArrayExpression: "off",
+                ObjectExpression: "off",
+            },
         ],
         "max-len": ["error", {
             code: 120,
             ignoreComments: true,
         }],
-        curly: ["error", "multi-line"],
+        "curly": ["error", "multi-line"],
         "prefer-const": ["error"],
         "comma-dangle": ["error", {
             arrays: "always-multiline",
@@ -68,11 +56,6 @@ module.exports = {
         "operator-linebreak": ["off"],
         // We don't mind strange alignments in EOL comments
         "no-multi-spaces": ["error", { "ignoreEOLComments": true }],
-
-        // ESLint's built in `no-invalid-this` rule breaks with class properties
-        "no-invalid-this": "off",
-        // ...so we replace it with a version that is class property aware
-        "@babel/no-invalid-this": "error",
 
         "no-multiple-empty-lines": ["error", { "max": 1 }],
         "object-curly-spacing": ["error", "always"]
