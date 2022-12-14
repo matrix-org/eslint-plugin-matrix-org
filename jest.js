abfc13c5d61c59d217c1e1148b2fea12117cc8f6
Eric Eastwood <erice@element.io>
2022-12-14 04:28:53 -0600

    Add some sane rule customization

--- jest.js
+++ jest.js
@@ -1,8 +1,20 @@
 module.exports = {
-  plugins: [
-    "jest"
-  ],
-  extends: [
-    "plugin:jest/recommended",
-  ],
-}
+    plugins: ["jest"],
+    extends: ["plugin:jest/recommended"],
+    rules: {
+        "jest/no-standalone-expect": [
+            "error",
+            {
+                additionalTestBlockFunctions: [
+                    // Tested and assertions in `before`/`beforeEach` blocks will fail
+                    // the test as expected
+                    "before",
+                    "beforeEach",
+                ],
+            },
+        ],
+        // It's ok to have no expect assertions in your test. Maybe you're testing that
+        // things just don't blow up.
+        "jest/expect-expect": "off",
+    },
+};
