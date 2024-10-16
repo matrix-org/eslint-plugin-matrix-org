7a17dd1062fddf03201c9492cf7b23f9a2f8db01
Michael Telatynski <7t3chguy@gmail.com>
2024-10-16 16:39:49 +0100

    Fix react rules

--- react.js
+++ react.js
@@ -2,12 +2,11 @@ module.exports = {
     plugins: [
         "deprecate",
         "matrix-org",
-        "@stylistic",
+        "react",
         "react-hooks",
     ],
     extends: [
         "prettier",
-        "plugin:@stylistic/disable-legacy",
     ],
     parserOptions: {
         ecmaFeatures: {
@@ -20,35 +19,35 @@ module.exports = {
 
         // This just uses the React plugin to help ESLint known when
         // variables have been used in JSX
-        "@stylistic/jsx-uses-vars": ["error"],
+        "react/jsx-uses-vars": ["error"],
         // Don't mark React as unused if we're using JSX
-        "@stylistic/jsx-uses-react": ["error"],
+        "react/jsx-uses-react": ["error"],
 
         // Components in JSX should always be defined
-        "@stylistic/jsx-no-undef": ["error"],
+        "react/jsx-no-undef": ["error"],
 
         // Assert spacing before self-closing JSX tags, and no spacing before
         // or after the closing slash, and no spacing after the opening
         // bracket of the opening tag or closing tag.
         // https://github.com/yannickcr/eslint-plugin-react/blob/HEAD/docs/rules/jsx-tag-spacing.md
-        "@stylistic/jsx-tag-spacing": ["error", {
+        "react/jsx-tag-spacing": ["error", {
             beforeClosing: "never",
         }],
 
-        "@stylistic/jsx-curly-spacing": ["error", "never", { allowMultiline: true }],
+        "react/jsx-curly-spacing": ["error", "never", { allowMultiline: true }],
 
-        "@stylistic/jsx-curly-brace-presence": ["error", "never"],
+        "react/jsx-curly-brace-presence": ["error", "never"],
 
-        "@stylistic/jsx-equals-spacing": ["error", "never"],
+        "react/jsx-equals-spacing": ["error", "never"],
 
-        "@stylistic/no-direct-mutation-state": ["error"],
-        "@stylistic/no-this-in-sfc": ["error"],
-        "@stylistic/self-closing-comp": ["error"],
-        "@stylistic/jsx-max-props-per-line": ["error", { "when": "multiline" }],
+        "react/no-direct-mutation-state": ["error"],
+        "react/no-this-in-sfc": ["error"],
+        "react/self-closing-comp": ["error"],
+        "react/jsx-max-props-per-line": ["error", { "when": "multiline" }],
 
         "react-hooks/rules-of-hooks": ["error"],
         "react-hooks/exhaustive-deps": ["error"],
-        "@stylistic/no-unknown-property": ["error"],
+        "react/no-unknown-property": ["error"],
 
         "deprecate/import": [
             "error",
